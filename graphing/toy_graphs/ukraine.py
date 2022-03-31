036419fc014cc9621317c496ab25cfbd8dae274d
Rohit Pandey <rohitpandey@Rohits-MacBook-Air.local>
2022-03-30 22:08:18 -0700

    added ukraine cities graph

---
+++ graphing/toy_graphs/ukraine.py
@@ -0,0 +1,33 @@
+
+
+ukr_grph_ts = {
+		'staging-1': {'chernihiv': 5},
+		'chernihiv': {'brovary': 4},
+		'brovary': {'kyiv': 1},
+		'boryspil': {'kyiv': 1.3, 'preiaslav': 2.3},
+		'preiaslav': {'boryspil': 3.3, 'kremanchuk': 2.5},
+		'kremanchuk': {'cherkasy': 2.3, 'preiaslav': 3.1, 'poltava': 2.6, 'dnipro': 2.4, 'kropyv': 2.0},
+		'cherkasy': {'kyiv': 3.2, 'kremanchuk': 2.2},
+		'bila_tservka': {'kyiv': 3.1, 'uman': 2.5},
+		'irpin': {'kyiv': 5.0, 'zhytomyr': 2.0},
+		'kyiv': {'brovary': 1.0, 'irpin': 1.0, 'bila_tservka': 1.8, 'cherkasy': 2.0, 'boryspil': 1.4, 'pyryatyn': 2.1},
+		'uman': {'vinnytsia': 2.0, 'odesa': 2.2, 'kropyv': 2.1, 'bila_tservka': 2.1},
+		'vinnytsia': {'zhytomyr': 1.7, 'uman': 1.5, 'odesa': 2.1},
+		'odesa': {'vinnytsia': 2.4, 'uman': 2.1, 'mykolaiv': 1.8},
+		'kropyv': {'kryvi_rih': 1.2, 'kremanchuk': 1.5, 'uman': 2.2, 'mykolaiv': 1.8},
+		'dnipro': {'kryvi_rih': 1.9, 'zaporizhzhia': 1.1, 'pavlohrad': 1.6, 'kharkiv': 2.1, 'kremanchuk': 2.3},
+		'staging-2': {'sumy': 1.4},
+		'sumy': {'kyiv': 4.5, 'kharkiv': 2.4},
+		'staging-3': {'kharkiv': 0.8},
+		'kharkiv': {'sumy': 1.9, 'okhtyrka': 1.5, 'poltova': 1.5, 'dnipro': 2.8, 'luhansk': 2.2},
+		'staging-4': {'luhansk': 0.4},
+		'staging-5': {'donetsk': 0.5},
+		'luhansk': {'kharkiv': 1.7, 'donetsk': 0.8},
+		'donetsk': {'mariupol': 1.4, 'zaporizhzhia': 1.8, 'pokrovsk': 1.1, 'luhansk': 2.0},
+		'mariupol': {'berdyansk': 0.4, 'donetsk': 0.7},
+		'berdyansk': {'mariupol': 0.4, 'melitopol': 0.5, 'zaporizhzhia': 1.4},
+		'staging-6': {'melitopol': 0.1},
+		'staging-7': {'kherson': 0.3},
+		'kherson': {'mykolaiv': 1.0, 'melitopol': 1.3},
+		'mykolaiv': {'odesa': 1.3, 'kherson': 1.1, 'kryvi_rih': 0.9, 'kropyv': 0.85}
+		}
