0d171e3ee47b2ed4ae63cb79952217826ed6d7c1
Akshay Sathiya <asathiya@microsoft.com>
2022-09-28 16:27:13 -0700

    added test cases for annealing code

--- tests/anneal_real_graph.py
+++ tests/anneal_real_graph.py
@@ -77,3 +77,64 @@ def tst():
     print("Score for first approach: " + str(scr1))
     print("####################")
     print("Score for second approach: " + str(scr2))
+
+def tst_input_edges(edges1, edges2): 
+    probs_l = even_probs(edges1[::, 0])
+    probs_c = even_probs(edges1[::, 1])
+    probs_r = even_probs(edges2[::, 1])
+
+    # Coverage schedule.
+    paths1 = min_cover_trigraph(edges1, edges2)
+    paths2 = np.array(complete_paths(paths1, edges1, edges2))
+    flow_dict_cov = path_arr_to_flow_dict(paths2)
+
+    n = 300
+    # Approach-1
+    qs_l, qs_c, qs_r = get_residual_targets(paths2, probs_l,
+                                            probs_c, probs_r, n)
+
+    ev = Evolutor(qs_l, qs_c, qs_r,
+                  edges1, edges2,
+                  n-len(paths2))
+    ev.anneal(presv_cov=False, n_iter=5000)
+
+    final_pths = add_path_dicts(ev.best_dict, flow_dict_cov)
+
+    scr1 = score(final_pths, probs_l, probs_c, probs_r)
+
+    # Approach-2
+    # First create a random set of paths above coverage.
+    flow_dict_init = copy.deepcopy(flow_dict_cov)
+
+    for _ in range(n-len(paths2)):
+        flow_dict_init = add_one_path(flow_dict_init, edges1, edges2)
+
+    ev1 = Evolutor(probs_l, probs_c, probs_r,
+                   edges1, edges2, n,
+                   start_dict=flow_dict_init)
+
+    ev1.anneal(presv_cov=True, n_iter=5000)
+
+    scr2 = ev1.min_score
+
+    return scr1, scr2
+
+if __name__=='__main__':
+    edges = [
+        (np.array([[1,4],[2,4],[2,5],[3,5]]), np.array([[4,6],[4,7],[5,8]])),
+        (np.array([[1,5],[2,5],[3,7],[4,6]]), np.array([[5,8],[5,9],[5,10],
+            [7,11],[6,11]]))
+    ]
+    scrs = []
+    for edges1, edges2 in edges:
+        scr1, scr2 = tst_input_edges(edges1, edges2)
+        scrs.append((scr1, scr2))
+
+    print()
+    for i in range(len(edges)): 
+        print(f'TEST CASE {i+1}')
+        print(f'Edges 1: {edges[i][0]}')
+        print(f'Edges 2: {edges[i][1]}')
+        print(f'Approach 1 score: {scrs[i][0]}')
+        print(f'Approach 2 score: {scrs[i][1]}')
+        print('###################')
