8439eb9215067d70846d6382814b8389b5bb6683
Akshay Sathiya <asathiya@microsoft.com>
2022-11-18 15:11:29 -0800

    updated sim anneal function params, and other minor changes

--- tests/simulated_annealing.py
+++ tests/simulated_annealing.py
@@ -1,25 +1,25 @@
-import numpy as np
 from graphing.special_graphs.neural_trigraph.marginal_matching.anneal\
     .simulated_annealing import simulated_annealing
 
 TEST_CASES = [
-    # edges1, edges2, complete_path_cover, number of nodes, simulated annealing 
-    # choice (0: first approach, 1: second approach, else best of 0 and 1)
+    # edges1, edges2, complete_path_cover, probs_l, probs_c, probs_r, number of paths, 
+    # simulated annealing choice (0: first approach, 1: second approach, else best of 0 and 1)
     ([[1,4],[2,4],[2,5],[3,5]], [[4,6],[4,7],[5,8]], [[1, 4, 7], [2, 4, 6], 
-        [3, 5, 8]], 300, 0),
+        [3, 5, 8]], None, None, None, 300, 0),
     ([[1,4],[2,4],[2,5],[3,5]], [[4,6],[4,7],[5,8]], [[1, 4, 7], [2, 4, 6], 
-        [3, 5, 8]], 300, 1),
+        [3, 5, 8]], None, None, None, 300, 1),
     ([[1,4],[2,4],[2,5],[3,5]], [[4,6],[4,7],[5,8]], [[1, 4, 7], [2, 4, 6], 
-        [3, 5, 8]], 300, 2),
+        [3, 5, 8]], None, None, None, 300, 2),
 ]
 
 PASSES = []
 FAILS = []
 for i in range(len(TEST_CASES)):
     print('TEST CASE', i+1)
-    edges1, edges2, complete_path_cover, num_nodes, sa_choice = TEST_CASES[i]
+    (edges1, edges2, complete_path_cover, probs_l, probs_c, probs_r, 
+        num_nodes, sa_choice) = TEST_CASES[i]
     path_counts, scr = simulated_annealing(edges1, edges2, complete_path_cover, 
-        num_nodes, sa_choice)
+        probs_l, probs_c, probs_r, num_nodes, sa_choice)
     print('Path Counts:', path_counts, '\nScore:', scr)
     try: 
         # check for coverage 
