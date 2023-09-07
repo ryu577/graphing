c2c5a9a0a7496d50146ffc8dfe02bf4110c7a644
Akshay Sathiya <asathiya@microsoft.com>
2023-09-07 13:17:53 -0700

    bug fix

---
+++ graphing/graph_utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+import networkx as nx
+
+
+def create_graph(edges):
+    """
+    Convert a collection of edges into networkx graph.
+    """
+    g = nx.Graph()
+    for i in edges:
+        g.add_edge(i[0], i[1], capacity=np.inf, weight=1)
+    return g
+
+
+def min_edge_cover(edges):
+    g = create_graph(edges)
+    mc = nx.min_edge_cover(g)
+    res = set()
+    for i in mc:
+        res.add((min(i), max(i)))
+    return res
